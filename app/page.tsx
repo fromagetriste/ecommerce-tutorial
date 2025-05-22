b97142fd7a1d7d2d0ad43b35b3e91146964c8402
fromagetriste __ <fromagetriste@gmail.com>
2025-05-22 15:31:59 -0600

    changed the home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,26 @@
-export default function Home({
-  children,
-}: Readonly<{ children: React.ReactNode }>) {
-  return <div>home page</div>;
+import { Button } from "@/components/ui/button";
+import { stripe } from "@/lib/stripe";
+import Link from "next/link";
+
+export default async function Home() {
+  const products = await stripe.products.list({
+    expand: ["data.default_price"],
+    limit: 5,
+  });
+  // console.log(products)
+  return (
+    <div>
+      <section>
+        <div>
+          <div>
+            <h2>Welcome to my Ecommerce</h2>
+            <p>Discover the latest products</p>
+            <Button asChild variant="default">
+              <Link href="/products">Browse all products</Link>
+            </Button>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 }
- 
