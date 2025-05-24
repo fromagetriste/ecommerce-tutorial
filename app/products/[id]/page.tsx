0c85c9e3a5f30c222d3269b6029c6d33212ce602
fromagetriste __ <fromagetriste@gmail.com>
2025-05-24 11:51:49 -0600

    added store with zustand

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,13 +1,17 @@
-import { ProductDetail } from "@/components/product-detail";
+import { ProductList } from "@/components/product-list";
 import { stripe } from "@/lib/stripe";
 
-export default async function ProductPage({
-  params,
-}: {
-  params: { id: string };
-}) {
-  const product = stripe.products.retrieve(params.id, {
-    expand: ["default_price"],
+export default async function ProductsPage() {
+  const products = await stripe.products.list({
+    expand: ["data.default_price"],
   });
-  return <ProductDetail product={product} />;
+
+  return (
+    <div className="pb-8">
+      <h1 className="text-3xl font-bold leading-none tracking-tight text-foreground text-center mb-8">
+        All Products
+      </h1>
+      <ProductList products={products.data} />
+    </div>
+  );
 }
